0b2d7707b44e10ff5f0b9a6add2e45294e0c47da
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-05 05:48:22 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -23,7 +23,6 @@ def remove_strawberry(contacts)
 
       if attribute == :favorite_ice_cream_flavors
       value.each do |flavor|
-        # here, each index element in an ice cream flavor string
         puts "#{flavor}"
       end
     end
